aeb2f28e29bb29a12d7747d0208badf786fd294a
James Martin <james.martin@bugsnag.com>
2023-07-25 10:36:32 +0100

    Promises promises

--- server/migrations/20230723093000_links_target_length.ts
+++ server/migrations/20230723093000_links_target_length.ts
@@ -2,11 +2,9 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<any> {
 
-  await Promise.all([
-    knex.raw(`
-        ALTER TABLE links ALTER COLUMN target TYPE VARCHAR;
-    `),
-  ]);
+  await knex.raw(`
+    ALTER TABLE links ALTER COLUMN target TYPE VARCHAR;
+  `);
 }
 
 export async function down(): Promise<any> {
