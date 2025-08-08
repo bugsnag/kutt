853da4d3933a346107b9500c722a13fd2722e365
xiaowei liu <xiaowei.liu@smartbear.com>
2025-08-08 14:54:46 -0400

    update deps

--- server/queues/visit.ts
+++ server/queues/visit.ts
@@ -1,38 +1,37 @@
-import useragent from "useragent";
+import { UAParser } from "ua-parser-js";
 import geoip from "geoip-lite";
 import URL from "url";
-
 import query from "../queries";
 import { getStatsLimit, removeWww } from "../utils";
 
 const browsersList = ["IE", "Firefox", "Chrome", "Opera", "Safari", "Edge"];
 const osList = ["Windows", "Mac OS", "Linux", "Android", "iOS"];
-const filterInBrowser = (agent) => (item) =>
-  agent.family.toLowerCase().includes(item.toLocaleLowerCase());
-const filterInOs = (agent) => (item) =>
-  agent.os.family.toLowerCase().includes(item.toLocaleLowerCase());
 
-export default function visit({ data }) {
-  const tasks = [];
+const filterInBrowser = (browserName: string) => (item: string) =>
+  browserName.toLowerCase().includes(item.toLowerCase());
+const filterInOs = (osName: string) => (item: string) =>
+  osName.toLowerCase().includes(item.toLowerCase());
 
-  tasks.push(query.link.incrementVisit({ id: data.link.id }));
+export default function visit({ data }: { data: any }) {
+  const tasks = [query.link.incrementVisit({ id: data.link.id })];
 
   if (data.link.visit_count < getStatsLimit()) {
-    const agent = useragent.parse(data.headers["user-agent"]);
-    const [browser = "Other"] = browsersList.filter(filterInBrowser(agent));
-    const [os = "Other"] = osList.filter(filterInOs(agent));
-    const referrer =
-      data.referrer && removeWww(URL.parse(data.referrer).hostname);
+    const parser = new UAParser(data.headers["user-agent"]);
+    const browserName = parser.getBrowser().name || "Other";
+    const osName = parser.getOS().name || "Other";
+    const [browser = "Other"] = browsersList.filter(filterInBrowser(browserName));
+    const [os = "Other"] = osList.filter(filterInOs(osName));
+    const referrer = data.referrer ? removeWww(URL.parse(data.referrer).hostname) : undefined;
     const location = geoip.lookup(data.realIP);
-    const country = location && location.country;
+    const country = location?.country || "Unknown";
     tasks.push(
       query.visit.add({
         browser: browser.toLowerCase(),
-        country: country || "Unknown",
+        country,
         id: data.link.id,
         os: os.toLowerCase().replace(/\s/gi, ""),
-        referrer: (referrer && referrer.replace(/\./gi, "[dot]")) || "Direct"
-      })
+        referrer: referrer ? referrer.replace(/\./gi, "[dot]") : "Direct"
+      }).then(() => 1)
     );
   }
 
