9607c4c445b409cee261f73a3570748a72347e55
Sufyan Ahmed <sufyan.ahmed@smartbear.com>
2025-12-10 12:59:18 +0000

    INFRA-5630 update kutt dependencies

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,19 @@
-import env from "./server/env";
+const env = require("./production-server/env").default;
 
 module.exports = {
   production: {
     client: "postgresql",
     connection: {
       host: env.DB_HOST,
+      port: parseInt(env.DB_PORT, 10),
       database: env.DB_NAME,
       user: env.DB_USER,
-      port: env.DB_PORT,
       password: env.DB_PASSWORD,
       ssl: env.DB_SSL,
     },
     migrations: {
-      tableName: "knex_migrations",
-      directory: "server/migrations"
-    }
-  }
+      directory: "./production-server/migrations",
+      loadExtensions: [".js"]
+    },
+  },
 };
